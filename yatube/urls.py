358bfd6c72a0c3ff7685605042bacee6927eaac4
IvanNadeevets <48166638+IvanNadeevets@users.noreply.github.com>
2021-05-11 10:00:41 +1000

    Update urls.py

--- yatube/urls.py
+++ yatube/urls.py
@@ -1,18 +1,3 @@
-""" yatube URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URL conf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls import handler404, handler500  # noqa
 from django.conf.urls.static import static
@@ -20,16 +5,13 @@ from django.contrib import admin
 from django.contrib.flatpages import views
 from django.urls import include, path
 
-handler404 = "posts.views.page_not_found"  # noqa
-handler500 = "posts.views.server_error"  # noqa
+handler404 = "posts.views.page_not_found"
+handler500 = "posts.views.server_error"
 
 urlpatterns = [
-    # flatpages
     path('about/', include('django.contrib.flatpages.urls'), name="flatpages"),
-    # регистрация и авторизация
     path("auth/", include("users.urls")),
     path("auth/", include("django.contrib.auth.urls")),
-    # раздел администратора
     path("admin/", admin.site.urls),
 ]
 
@@ -39,7 +21,6 @@ urlpatterns += [
     path('about-author/', views.flatpage, {'url': '/about-author/'},
          name='author'),
     path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='spec'),
-    # импорт из приложения posts
     path("", include("posts.urls")),
 ]
 
